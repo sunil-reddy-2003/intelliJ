cbf36cd8c3158da68420bcb496e89c251aeb228d
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-06-17 23:52:08 +0530

    init commit

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,24 +1,31 @@
 public class BubbleSort {
     static void bubbleSort(int[] arr){
+
         for(int i=0; i<arr.length; i++){
+            boolean swapped=false;
             for(int j=1; j<arr.length-i; j++){
                 if(arr[j]<arr[j-1]){
                     int  temp=arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=temp;
+                    swapped=true;
                 }
             }
+            if(!swapped) break;
         }
     }
 
     public static void main(String[] args){
         int[] arr={5,4,3,2,1};
+//        int[] arr={1,2,3,4,5};
 
 //        String str="sunil";
 //        char[] arr=str.toCharArray();
+
         bubbleSort(arr);
         for (int c : arr) {
             System.out.print(c + " ");
         }
+
     }
 }
