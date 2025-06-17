8fcc916629dd43619e85b26a64dfc9a87b77c64e
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-06-18 00:31:33 +0530

    init commit

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -19,6 +19,7 @@ public class BubbleSort {
 //        int[] arr={5,4,3,2,1};
 //        int[] arr={1,2,3,4,5};
 //        int[] arr={};
+//        int[] arr={1};
 //        int[] arr={2,1};
         int[] arr={4,7,0,-22,-31};
 
---
+++ src/SelectionSort.java
@@ -0,0 +1,23 @@
+public class SelectionSort {
+
+    static void selectionSort(int[] arr){
+
+    }
+
+
+
+    public static void main(String[] args){
+//        int[] arr={5,4,3,2,1};
+//        int[] arr={1,2,3,4,5};
+//        int[] arr={};
+        int[] arr={1};
+//        int[] arr={2,1};
+//        int[] arr={4,7,0,-22,-31};
+
+
+        selectionSort(arr);
+        for (int c : arr) {
+            System.out.print(c + " ");
+        }
+    }
+}
