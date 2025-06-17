6ba21cd3711108434895471019a4a60073965436
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-06-18 00:11:14 +0530

    init commit

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -16,8 +16,11 @@ public class BubbleSort {
     }
 
     public static void main(String[] args){
-        int[] arr={5,4,3,2,1};
+//        int[] arr={5,4,3,2,1};
 //        int[] arr={1,2,3,4,5};
+//        int[] arr={};
+//        int[] arr={2,1};
+        int[] arr={4,7,0,-22,-31};
 
 //        String str="sunil";
 //        char[] arr=str.toCharArray();
