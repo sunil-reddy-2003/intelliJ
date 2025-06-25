2d9e88f4631765d7fddd560bd95f6cd658d1e924
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-06-25 23:07:26 +0530

    init commit

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -1,20 +1,49 @@
 package sorting;
 
 public class MergeSort {
-    static int[] mergeSort(int[] arr1,int[] arr2){
-        int[] merged=new int[arr1.length+ arr2.length];
-        for(int i=0; i< merged.length; i++){
-            if(arr1[i]<arr2[i]){
-                merged[i]=arr1[i];
+    static void mergeSort(int[] arr,int s,int e){
+        if (e - s <= 1) return;
+
+        int mid=s+(e-s)/2;
+        mergeSort(arr,s,mid);
+        mergeSort(arr,mid,e);
+
+        mergeArrays(arr,s,mid,e);
+    }
+    static void mergeArrays(int[] arr,int s,int m,int e){
+        int i=s;
+        int j=m;
+        int k=0;
+        int[] merged=new int[e-s];
+        while(i< m && j< e){
+            if(arr[i]<arr[j]){
+                merged[k]=arr[i];
+                i++;
+                k++;
+            }else{
+                merged[k]=arr[j];
+                j++;
+                k++;
             }
         }
-        return new int[arr1.length+ arr2.length];
+        while(i< m){
+                merged[k]=arr[i];
+                i++;
+                k++;
+        }
+        while(j<e){
+            merged[k]=arr[j];
+            j++;
+            k++;
+        }
+        for (int l=0; l<merged.length; l++){
+            arr[s+l]=merged[l];
+        }
     }
     public static void main(String[] args){
-        int[] arr1={1,3,5,7,9};
-        int[] arr2={2,4,6,8,10};
-        int[] arr3=mergeSort(arr1,arr2);
-        for(int num:arr3){
+        int[] arr={1,3,5,7,9,2,4,6,8,10};
+        mergeSort(arr,0,arr.length-1);
+        for(int num:arr){
             System.out.print(num+" ");
         }
     }
