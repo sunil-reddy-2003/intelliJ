61c92283ddc51908e1cdfc894fba453d34b1c319
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-07-09 19:32:04 +0530

    init commit

---
+++ src/oops/Basic.java
@@ -0,0 +1,34 @@
+package oops;
+class Area{
+    int length;
+    int breadth;
+    int height;
+
+    public Area() {
+        System.out.println("in constructor");
+    }
+
+    public Area(int length, int height, int breadth) {
+        this.length = length;
+        this.height = height;
+        this.breadth = breadth;
+    }
+
+    public Area(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    public Area(Area old) {
+        this.length = old.length;
+        this.breadth = old.breadth;
+    }
+}
+public class Basic {
+    public static void main(String[] args) {
+    Area area=new Area(22,3);
+        System.out.println("length :"+area.length+" breadth: "+area.breadth);
+        Area area2=new Area(area);
+        System.out.println("length :"+area.length+" breadth: "+area.breadth);
+        Area area3=new Area();
+    }
+}
