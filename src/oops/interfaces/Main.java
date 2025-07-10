8aaf9f9e767cfcac78a24140457287b7be1c91e6
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-07-10 17:25:57 +0530

    init commit

---
+++ src/oops/interfaces/Main.java
@@ -0,0 +1,32 @@
+package oops.interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+//        Car car = new Car();
+//        car.start();
+//        car.stop();
+//        car.acc();
+//        car.brake();
+
+
+        // which version to access will be mentioned by the object "new Car()"
+        // what to access will be mentioned by "Engine car"
+        //its type is Engine, so it can access only methods which are present in Engine
+//        Engine car = new Car();
+//        car.acc();
+//        car.start();
+//        car.stop();
+
+//        car.brake();
+//        ((Car) car).brake();
+
+//        Media carMedia = new Car();
+//        carMedia.stop();
+
+        NiceCar car= new NiceCar();
+        car.start();
+        car.startMusic();
+        car.upgradeEngine();
+        car.start();
+    }
+}
