1600d32b8fd1015e44587d991efbad44d92f6f23
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-07-16 11:04:20 +0530

    init commit

--- src/BasicPrograms/ArmstrongOrNot.java
+++ src/BasicPrograms/ArmstrongOrNot.java
@@ -1,7 +1,19 @@
 package BasicPrograms;
 
 public class ArmstrongOrNot {
+//    static int rem=0;
+    static int armstrong(int num,int power){
+//        rem+=(int )Math.pow(num%10,power);
+        if(num==0){
+            return 0;
+//            return rem;
+        }
+        return (int )Math.pow(num%10,power)+armstrong(num/10,power);
+    }
     public static void main(String[] args) {
-
+        int num=153;
+        int power=(int)Math.log10(num)+1;
+        int ans=armstrong(num,power);
+        System.out.println(num==ans);
     }
 }
