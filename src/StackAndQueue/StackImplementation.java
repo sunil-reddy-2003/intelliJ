4da69a89c14b2ba93b1b7675b64048657d6af567
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-07-17 23:35:14 +0530

    init commit

---
+++ src/StackAndQueue/StackImplementation.java
@@ -0,0 +1,19 @@
+package StackAndQueue;
+import java.util.Stack;
+public class StackImplementation {
+    public static void main(String[] args) {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println(stack);
+
+    }
+}
