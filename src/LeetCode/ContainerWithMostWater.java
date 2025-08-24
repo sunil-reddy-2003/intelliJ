06b16e8175ec2a41d9a807dd3013f6e453645c21
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-08-24 17:09:35 +0530

    init commit

---
+++ src/LeetCode/ContainerWithMostWater.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height){
+        int area=0;
+        int start=0;
+        int end= height.length-1;
+
+        while(start<end){
+            int temp=0;
+            temp=Math.min(height[start],height[end])*(end-start);
+            area=Math.max(area,temp);
+            if(height[start]<height[end]){
+                start++;
+            }else{
+                end--;
+            }
+        }
+
+        return area;
+    }
+    public static void main(String[] args) {
+        int[] height={1,8,6,2,5,4,8,3,7};
+//        int[] height={8,6,2,5,4,8};
+
+        int ans=maxArea(height);
+        System.out.println(ans);
+    }
+}
