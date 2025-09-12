cf0847a836251e86845bf9fa8e6c6e5343c1a7f7
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-09-12 20:39:07 +0530

    init commit

--- src/Recursion/Sequences/Permutations.java
+++ src/Recursion/Sequences/Permutations.java
@@ -1,23 +1,49 @@
 package Recursion.Sequences;
 
-public class Permutations {
+import java.util.*;
 
-    static void permutations(String pro,String un_pro){
-        if(un_pro.isEmpty()){
-            System.out.println(pro);
-            return;
+public class Permutations {
+    public static void main(String[] args) {
+        int[] arr={1,2,3};
+        boolean[] freq=new boolean[arr.length];
+//        permutations(arr,freq,new ArrayList<>());
+        List<List<Integer>> ans = new ArrayList<>();
+        swapPermutations(0,arr,ans);
+        System.out.println(ans);
+    }
+    private static void swapPermutations(int index,int[] arr,List<List<Integer>> ans){
+        if(index==arr.length){
+            List<Integer> list=new ArrayList<>();
+            for (int j : arr) {
+                list.add(j);
+            }
+            ans.add(list);
         }
-        char ch=un_pro.charAt(0);
-        int size=pro.length();
-        for (int i = 0; i <= pro.length(); i++) {
-            String f= pro.substring(0,i);
-            String s=pro.substring(i,size);
-            permutations(f+ch+s,un_pro.substring(1));
+        for (int i = index; i < arr.length; i++) {
+            swap(i,index,arr);
+            swapPermutations(index+1,arr,ans);
+            swap(i,index,arr);
         }
     }
-
-    public static void main(String[] args) {
-        permutations("","abc");
+    static void swap(int i, int j,int[] arr){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    private static void permutations(int[] arr, boolean[] freq, ArrayList<Integer> list) {
+        if(list.size()==3){
+            System.out.println(list);
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(!freq[i]){
+                list.add(arr[i]);
+                freq[i]=true;
+                permutations(arr,freq,list);
+                list.remove(list.size()-1);
+                freq[i]=false;
+            }
+        }
     }
 
 }
